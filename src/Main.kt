de1cc812eaa8b3e49cb5ff3870b3af2070716ffe
William Barras <williamtbarras@gmail.com>
2019-10-20 15:14:08 -0600

    Add notes on how the Euclidean algorithm works

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val numberTheory = NumberTheory()
 
-    var gcd = numberTheory.greatestCommonDivisor(12, 7)
+    var gcd = numberTheory.greatestCommonDivisor(12, 8)
     println(gcd)
 }
--- src/NumberTheory.kt
+++ src/NumberTheory.kt
@@ -1,7 +1,19 @@
 class NumberTheory() {
 
+
+    /*
+    greatestCommonDivisor:
+    This method finds the greatest common divisor of two integers a and b using the Euclidean Algorithm
+        1) Let r(0) = a and r(1) = b
+        2) Set i = 1
+        3) Divide r(i-1) by r(i) to get quotient q(i) and remainder r(i+1)
+                r(i+1) = r(i) * q(i) + r(i+1)      with 0 <= r(i+1) < r(i)
+        4) If the remainder r(i+1) == 0, then r(i) == gcd(a.b) and the algorithm terminates
+        5) Otherwise, r(i+1) > 0, so set i = i+1 and go to step 3)
+
+        Completes in at most 2log2(b) iterations
+     */
     fun greatestCommonDivisor(a : Int, b : Int) : Int {
-        var quotient : Int
         var rOne : Int = a
         var rTwo : Int = b
         var greatestCommonDivisor : Int = -1

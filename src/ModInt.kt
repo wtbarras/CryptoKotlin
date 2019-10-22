807414220afe5f03018be37ca0057474f19bb8ab
William Barras <williamtbarras@gmail.com>
2019-10-21 22:01:04 -0600

    Implement addition

--- src/ModInt.kt
+++ src/ModInt.kt
@@ -5,5 +5,13 @@ class ModInt(initialValue : Int, private val modulus : Int) {
             field %= this.modulus
         }
 
+    operator fun plus(b : ModInt) : ModInt{
+        val newValue = (this.value + b.value) % this.modulus
+        return ModInt(newValue, this.modulus)
+    }
+
+    override fun toString(): String {
+        return this.value.toString()
+    }
 
 }
